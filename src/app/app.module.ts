1bf1f342b6917e9ffdc589c0622418ad580fcf35
Bonfim Justino do Nascimento <bonfim1999@gmail.com>
2020-08-24 23:09:04 -0300

    Contents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContentComponent } from './components/content/content.component';
+import { ButtonComponent } from './components/shared/button/button.component';
+import { CardComponent } from './components/shared/card/card.component';
+import { BoxComponent } from './components/shared/box/box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ContentComponent
+    ContentComponent,
+    ButtonComponent,
+    CardComponent,
+    BoxComponent
   ],
   imports: [
     BrowserModule,
